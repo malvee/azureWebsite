0045d3a0880cb7229542f4c83fa00fc39f89bfd3
Momshad Alvee <momshadalvee@gmail.com>
2014-10-27 16:02:42 +0000

    2

--- index.php
+++ index.php
@@ -21,6 +21,7 @@
 <form method="post" action="index.php" enctype="multipart/form-data" >
       Name  <input type="text" name="name" id="name"/></br>
       Email <input type="text" name="email" id="email"/></br>
+      Company Name <input type="text" name="company_name" id="company_name"/></br>
       <input type="submit" name="submit" value="Submit" />
 </form>
 <?php
